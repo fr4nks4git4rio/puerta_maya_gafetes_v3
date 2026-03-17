28b8d16dc625f6e47e39ac2c9292488dfb9e9525
Frank Garcia <fr4nk.s4git4rio@gmail.com>
2026-03-17 18:46:33 -0500

    arreglos para el cron, que se ejecute cada 10 minutos

--- cron.bat
+++ cron.bat
@@ -2,7 +2,7 @@
 
 CD d: &&  CD \Job\PuertaMayaGafetes\proyecto\pm_credenciales && php artisan schedule:run
 
-timeout 10
+timeout 600
 
 CD d: &&  CD \Job\PuertaMayaGafetes\proyecto\pm_credenciales && "cron.bat"
 
